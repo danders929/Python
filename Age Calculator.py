98556703d5bd85f742f429b5de7192f193f0a130
danders929 <36702902+danders929@users.noreply.github.com>
2019-10-26 01:32:07 -0500

    Update changes to Age Calculator.py
    *Minor output fixes that make output more readable.
    *Defined user functions now return their correct value type.
    *Modified numCheck function to be more efficient and removed redundant code.
    *Fixed an issue that allowed user to input a negative number for their age. 
    *Fixed a bug that allowed user to input a number greater than 10 after first attempt.

--- Age Calculator.py
+++ Age Calculator.py
@@ -2,37 +2,34 @@ from datetime import date
 
 # User input functions. Each function has customized error handling
 def nameCheck():
-    name = input("enter name: ")
+    name = input("\nEnter name: ")
     if len(name)>32:
         name = input("\nPlease limit your name to 32 characters. \nWhat is your Name? ")
     return name
 def ageCheck():
-    age = input("How old are you? ")[:3]
+    age = input("How old are you? ")
     while True:
         try:
             age = int(age)
-            break
+            if age<1:
+                age = input("\nHow are you even alive right now? \n Please enter your age: ")
+            else:
+                break
         except ValueError:
-            age  = input("\nPlease use a numerical format. \nHow old are you? ")[:3]
-        break
-    return age
+            age  = input("\nPlease use a numerical format. \nHow old are you? ")
+    return int(age)
 def numCheck():
-    n = input("Enter a number between 1 and 10: ")[:2]
+    n = input("Enter a number between 1 and 10: ")
     while True:
         try:
             n = int(n)
-            if isinstance(n,int):
-                if n<1 or n>10:           
-                    n = input("\nPlease pick a number between 1 and 10: ")[:2]
-                elif n>1 and n<10:
-                    break
-                break
+            while n<1 or n>10:      
+                n = int(input("\nPlease pick a number between 1 and 10: "))
             else:
-                n = input("\nInput must be in numerical format. \nPlease pick a number between 1 and 10 :")[:2]
+                break
         except ValueError:
-            n = input("\nPlease pick a number between 1 and 10: ")[:2]
-        break
-    return n
+            n = input("\nInput must be in numerical format. \nPlease pick a number between 1 and 10 :")
+    return int(n)
 
 # Calls the various functions and assigns them to a variable. 
 name = nameCheck()
@@ -46,17 +43,17 @@ today = date.today()
 year = today.year
 y100 = year - int(age) + 100
 
-if int(age)>=120:
+if age>=120:
     print ("\nDamn you are the oldest person alive!\n" * n)
 
-elif int(age)>100 and age<120:
-    print ("\nCongratulations %s you made it past 100 on the year %s.\n" % (name,y100) * int(n))
+elif age>100 and age<120:
+    print ("\nCongratulations %s you made it past 100 on the year %s.\n" % (name,y100) * n)
 
-elif int(age)==100:
-    print ("\nCongratulations %s you turned 100 this year! \n" % (name) * int(n))
+elif age==100:
+    print ("\nCongratulations %s you turned 100 this year! \n" % (name) * n)
 
 else: 
-    print ("\nHi %s! \nDid you know, you will turn 100 on the year %s\n" % (name, y100) * int(n))
+    print ("\nHi %s! \nDid you know, you will turn 100 on the year %s\n" % (name, y100) * n)
 
 
      
